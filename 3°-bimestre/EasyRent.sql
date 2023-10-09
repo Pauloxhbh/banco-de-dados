c555091fba6bdf3bd38538e7597f2e32b49bc93d
Pauloxhbh <109999895+Pauloxhbh@users.noreply.github.com>
2023-10-08 21:50:56 -0300

    Update EasyRent.sql

--- 3°-bimestre/EasyRent.sql
+++ 3°-bimestre/EasyRent.sql
@@ -1,1 +1,21 @@
+questão 1
+SELECT p.* 
+FROM Propriedade p
+INNER JOIN ContratoAluguel c ON p.propriedade_id = c.propriedade_id
+WHERE c.inquilino_id = 1;
 
+questão 2
+não soube fazer
+
+questão 3 
+SELECT endereco, COUNT(*) as quantidade
+FROM Propriedade
+GROUP BY endereco;
+
+questão 4
+
+SELECT p.*, MAX(c.data_inicio) as data_pagamento
+FROM Propriedade p
+LEFT JOIN ContratoAluguel c ON p.propriedade_id = c.propriedade_id
+GROUP BY p.propriedade_id, p.endereco, p.tipo_propriedade, p.num_quartos, p.num_banheiros
+ORDER BY data_pagamento DESC NULLS LAST;
